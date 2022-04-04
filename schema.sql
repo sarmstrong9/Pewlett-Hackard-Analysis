5a5e74fa67901b642fd3111fc280ce93f07778e4
Armstrong <Scott.Armstrong@viasat.com>
2022-04-03 21:29:41 -0700

    tables added and retirements queries added

--- schema.sql
+++ schema.sql
@@ -41,15 +41,24 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 CREATE TABLE dept_employee (
-	dept_no VARCHAR (4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR (4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
+
+-- SELECT * FROM departments;
+-- SELECT * FROM employees;
+-- SELECT * FROM salaries;
+-- SELECT * FROM dept_employee;
+-- SELECT * FROM dept_manager;
+-- SELECT * FROM titles;
+
+-- DROP TABLE titles CASCADE;
