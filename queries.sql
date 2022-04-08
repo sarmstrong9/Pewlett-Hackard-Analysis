6081bd3cc22323c577c358471c4b3f68ff56cd16
Armstrong <Scott.Armstrong@viasat.com>
2022-04-08 06:01:45 -0700

    double join tables complete

--- queries.sql
+++ queries.sql
@@ -161,4 +161,39 @@ GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
 
+-- List 1 - Employee Information
+-- looking at details from salaries table
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- looking at creating a new table with employee info
+SELECT emp_no,
+    first_name,
+	last_name,
+    gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Didnt actually want to create my table yet as we are joininng and creating at the same time in the next step
+DROP TABLE emp_info;
+
+-- Joining 3 tables into 1 new table to include all data we need
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_employee as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
 
