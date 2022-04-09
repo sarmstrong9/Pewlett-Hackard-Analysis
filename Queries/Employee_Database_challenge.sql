25af8fa02d678661914e0d74d8c099509d91699e
Armstrong <Scott.Armstrong@viasat.com>
2022-04-09 12:31:37 -0700

    deliverable 1 complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -7,14 +7,13 @@ SELECT e.emp_no,
     t.title,
     t.from_date,
     t.to_date
--- INTO retirement_titles
+INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no ASC;
 
--- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 rt.first_name,
 rt.last_name,
@@ -23,4 +22,17 @@ INTO unique_titles
 FROM retirement_titles as rt
 WHERE (rt.to_date = '9999-01-01')
 ORDER BY emp_no, to_date DESC;
+	   
+SELECT * FROM unique_titles;
 
+SELECT COUNT (ut.title), ut.title
+INTO retirement_titles_count
+FROM unique_titles as ut
+GROUP BY (ut.title)
+ORDER BY count DESC;
+
+SELECT * FROM retirement_titles_count;
+
+
+	   
+	   
