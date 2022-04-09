592dbf3a03ff71a3a987b2f9b8c0471de87c0a9b
Armstrong <Scott.Armstrong@viasat.com>
2022-04-09 12:21:03 -0700

    Deliverable 1 progress

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,26 @@
+
+-- Challenge Deliverable 1
+
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    t.title,
+    t.from_date,
+    t.to_date
+-- INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Pewlett-Hackard-Analysis
+
+## Purpose
+
+
+- Determine the number of retiring employees per title
+- Identify employees who are eligible to participate in a mentorship program
